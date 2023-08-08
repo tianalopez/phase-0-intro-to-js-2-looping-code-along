9f0e37b6c3c85bbfe9d22f8c14d7c68131664442
Tiana Lopez <tianadavin@gmail.com>
2023-08-08 12:38:50 -0700

    Adds functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  const newArray = [];
+  for (let i = 0; i < names.length; i++) {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return newArray;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
