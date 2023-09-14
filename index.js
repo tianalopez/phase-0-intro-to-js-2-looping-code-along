45e6be4b6f9bc99d0c9592a93331fd7f7ea61fa5
Tiana Lopez <tianadavin@gmail.com>
2023-09-13 22:10:21 -0700

    redid code

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // Code your solutions in this file
-function writeCards(names, event) {
+// function writeCards(names, event) {
+//   const newArray = [];
+//   for (let i = 0; i < names.length; i++) {
+//     newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+//   }
+//   return newArray;
+// }
+
+// function countDown(number) {
+//   while (number >= 0) {
+//     console.log(number);
+//     number--;
+//   }
+// }
+
+// redoing the code
+function writeCards(array, event) {
   const newArray = [];
-  for (let i = 0; i < names.length; i++) {
-    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
   }
   return newArray;
 }
